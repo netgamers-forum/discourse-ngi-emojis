6b70a11e2fd417b640822282eb3124a2366dd4ec
Crius <claudio.vallesi@gmail.com>
2023-02-06 17:12:09 +0000

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -191,5 +191,5 @@ after_initialize do
     register_emoji "yawn", "/plugins/discourse-ngi-emojis/images/yawn.gif", "ngi"
     register_emoji "yotsu", "/plugins/discourse-ngi-emojis/images/yotsu.gif", "ngi"
     register_emoji "shocked", "/plugins/discourse-ngi-emojis/images/zx11shocked.gif", "ngi"
-    register_emoji "zzz", "/plugins/discourse-ngi-emojis/images/zzz.gif", "ngi"
+    register_emoji "oldzzz", "/plugins/discourse-ngi-emojis/images/zzz.gif", "ngi"
 end
